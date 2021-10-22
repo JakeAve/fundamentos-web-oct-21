e246bffb456666d73132b83faff1e607593b6860
JakeAve <jacobjavery@gmail.com>
2021-10-21 19:55:57 -0500

    clase de 21 de oct

---
+++ html/algoritmos1.js
@@ -0,0 +1,51 @@
+// for (inicio; terminación; cambio)
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+
+//   // i = 5
+// }
+// console.log("terminado");
+
+// i -> 0
+// i -> 1
+// i -> 2
+// i -> 3
+// i -> 4
+// terminado
+
+// for (let i = 0; i < 5; i++) {
+//   i = i + 1;
+//   console.log(i);
+// }
+
+// i -> 0
+// i -> 1
+// console de 1
+
+// i -> 2
+// i -> 3
+// console de 3
+
+// i -> 4
+// i -> 5
+// console de 5
+
+// i -> 6
+// termina bucle
+
+a = 15;
+console.log(a);
+function y(a) {
+  console.log(a);
+  return a * 2;
+}
+b = y(10);
+console.log(b);
+
+// a -> 15
+// console de 15
+// // otro a -> 10
+// // console de 10
+// b -> 20
+// console de 20
